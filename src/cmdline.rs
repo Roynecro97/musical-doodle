26c11374d34f024f82caf02926f9c469ced5a971
Roy Abudi <Roynecro97@users.noreply.github.com>
2023-09-19 23:26:48 +0300

    Add empty modules & initial commandline parsing

---
+++ src/cmdline.rs
@@ -0,0 +1,155 @@
+use std::{path::PathBuf, str::FromStr};
+
+use structopt::{clap::AppSettings, StructOpt};
+
+#[derive(Debug, StructOpt)]
+pub enum Music {
+    Song {
+        #[structopt(required = true, min_values = 1)]
+        songs: Vec<String>,
+    },
+    Playlist {
+        playlist: String,
+    },
+    AllSongs,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Play {
+    #[structopt(long)]
+    pub shuffled: bool,
+
+    #[structopt(long)]
+    pub repeat: bool,
+
+    #[structopt(subcommand)]
+    pub command: Option<Music>,
+}
+
+
+#[derive(Debug, StructOpt)]
+pub struct Queue {
+    #[structopt(long)]
+    pub shuffled: bool,
+
+    #[structopt(subcommand)]
+    pub command: Music,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum ClientCommand {
+    /// TODO: add docs
+    Play(Play),
+
+    /// Add to music queue
+    Queue(Queue),
+
+    /// Pause currently playing music
+    Pause,
+
+    /// Query the server for the currently playing song
+    Status,
+
+    /// Tell the server to exit
+    Shutdown,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Client {
+    #[structopt(subcommand)]
+    pub command: ClientCommand,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Server {
+    /// Music library path
+    pub path: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Command {
+    Client(Client),
+    Server(Server),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warning,
+    Critical,
+}
+
+impl FromStr for LogLevel {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "debug" => Ok(Self::Debug),
+            "info" => Ok(Self::Info),
+            "warning" => Ok(Self::Warning),
+            "critical" => Ok(Self::Critical),
+            _ => Err("valid values: debug, info, warning, critical"),
+        }
+    }
+}
+
+impl AsRef<str> for LogLevel {
+    fn as_ref(&self) -> &'static str {
+        match self {
+            Self::Debug => "debug",
+            Self::Info => "info",
+            Self::Warning => "warning",
+            Self::Critical => "critical",
+        }
+    }
+}
+
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    about = "Remote music player",
+    global_settings = &[
+        AppSettings::ColoredHelp,
+        AppSettings::VersionlessSubcommands,
+        AppSettings::DeriveDisplayOrder,
+        AppSettings::DisableHelpSubcommand,
+    ],
+)]
+pub struct Opt {
+    /// The path to the logfile
+    #[structopt(long)]
+    pub logfile: Option<PathBuf>,
+
+    /// Suppress stdout/stderr log output (logfile is still written to if provided)
+    #[structopt(short, long)]
+    pub quiet: bool,
+
+    /// Easy override of log level from the command line.
+    /// The value values are: debug, info, warning, critical.
+    #[structopt(long, default_value = "info")]
+    pub log_level: LogLevel,
+
+    /// When running as a server, this is the adddress to listen on.
+    /// The server will listen on all interfaces if not specified.
+    ///
+    /// When running as client, this is the server address of the Coordinator
+    /// to connect to.
+    #[structopt(short = "a", long)]
+    pub server_address: Option<String>,
+
+    /// When running as a server, this is the port to listen to.
+    /// Will be picked at random and printed if not specified.
+    ///
+    /// When running as a client, this is the port to use to connect.
+    #[structopt(short = "p", long)]
+    pub server_port: Option<u16>,
+
+    #[structopt(subcommand)]
+    pub command: Command,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::{io::BufReader, fs::File};
+#![allow(unused_imports)]
+pub(crate) mod client;
+pub(crate) mod cmdline;
+pub mod common;
+pub(crate) mod server;
 
-use rodio::{OutputStream, Decoder, Sink};
-use serde_derive::{Serialize, Deserialize};
+use std::{fs::File, io::BufReader};
+
+use rodio::{Decoder, OutputStream, Sink};
+use serde_derive::{Deserialize, Serialize};
 use structopt::{clap::AppSettings, StructOpt};
 
 #[derive(Debug, Clone, Serialize, Deserialize, StructOpt)]
@@ -22,30 +28,40 @@ struct Opt {
 fn main() -> color_eyre::eyre::Result<()> {
     color_eyre::install()?;
 
-    let opt = Opt::from_args();
-    println!("Hello, world!");
-    println!("My configuration is {:?}", opt);
-
-    use rodio::cpal::traits::{HostTrait, DeviceTrait};
-    let host = rodio::cpal::default_host();
-    println!("default: {:?}", host.default_output_device().map(|d| d.name().unwrap_or("missing name".to_owned())));
-    for (i, device) in host.output_devices()?.enumerate() {
-        println!("{}: {}", i, device.name().unwrap_or("missing name".to_owned()));
+    #[cfg(not(target_feature="play-single-file"))]
+    {
+        let opt = cmdline::Opt::from_args();
+        println!("{:?}", opt);
+        println!("{}", opt.log_level);
     }
 
-    let (_stream, stream_handle) = OutputStream::try_default()?;
-    let sink = Sink::try_new(&stream_handle)?;
-    sink.set_volume(0.1);
+    #[cfg(target_feature="play-single-file")]
+    {
+        let opt = Opt::from_args();
+        println!("Hello, world!");
+        println!("My configuration is {:?}", opt);
 
-    println!("paused: {}", sink.is_paused());
-    println!("volume: {}", sink.volume());
-    println!("speed: {}", sink.speed());
+        use rodio::cpal::traits::{DeviceTrait, HostTrait};
+        let host = rodio::cpal::default_host();
+        println!("default: {:?}", host.default_output_device() .map(|d| d.name().unwrap_or("missing name".to_owned())));
+        for (i, device) in host.output_devices()?.enumerate() {
+            println!("{}: {}", i, device.name().unwrap_or("missing name".to_owned()));
+        }
 
-    for _ in 0..opt.times {
-        sink.append(Decoder::new(BufReader::new(File::open(&opt.soundfile)?))?);
-        sink.play();
-        println!("playing...");
-        sink.sleep_until_end();
+        let (_stream, stream_handle) = OutputStream::try_default()?;
+        let sink = Sink::try_new(&stream_handle)?;
+        sink.set_volume(0.1);
+
+        println!("paused: {}", sink.is_paused());
+        println!("volume: {}", sink.volume());
+        println!("speed: {}", sink.speed());
+
+        for _ in 0..opt.times {
+            sink.append(Decoder::new(BufReader::new(File::open(&opt.soundfile)?))?);
+            sink.play();
+            println!("playing...");
+            sink.sleep_until_end();
+        }
     }
 
     Ok(())
